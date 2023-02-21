7f1b0a7c48aebf2d2e10c71bef0b1417bd2122e8
Ian Chirwa <chirwaian7@gmail.com>
2023-02-21 16:46:38 +0200

    Revert "modified several files"
    This reverts commit 9682a50fd48c51c6b2e58ea5c986d76bd6001f16.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,29 +1,42 @@
-#include "sort.h"
-
+include "sort.h"
+#include <stddef.h>
 /**
-* selection_sort - sorts array using selection sort
-* @array: array to be sorted
-* @size: Size of array
-*/
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
+ **/
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * selection_sort - sort array using selection sort algorithm
+ * @array: array
+ * @size: array size
+ **/
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, t, min;
+	unsigned int i, j, min;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min])
+		{
+			if (array[min] > array[j])
 				min = j;
+		}
 		if (min != i)
 		{
-			t = array[min];
-			array[min] = array[i];
-			array[i] = t;
+			_swap(&array[i], &array[min]);
 			print_array(array, size);
 		}
 	}
