9682a50fd48c51c6b2e58ea5c986d76bd6001f16
Ian Chirwa <chirwaian7@gmail.com>
2023-02-21 16:34:01 +0200

    modified several files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,29 @@
-include "sort.h"
-#include <stddef.h>
-/**
- * _swap - swap two numbers
- * @a: integer
- * @b: integer
- **/
-void _swap(int *a, int *b)
-{
-	int tmp;
+#include "sort.h"
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
 /**
- * selection_sort - sort array using selection sort algorithm
- * @array: array
- * @size: array size
- **/
+* selection_sort - sorts array using selection sort
+* @array: array to be sorted
+* @size: Size of array
+*/
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min;
+	size_t i, j, t, min;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
-		{
-			if (array[min] > array[j])
+			if (array[j] < array[min])
 				min = j;
-		}
 		if (min != i)
 		{
-			_swap(&array[i], &array[min]);
+			t = array[min];
+			array[min] = array[i];
+			array[i] = t;
 			print_array(array, size);
 		}
 	}
